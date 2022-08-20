77415984dc1f1a38d3087c02072179191a34add6
DevSSH <sepehr86.HomeWork@gmail.com>
2022-08-20 15:17:29 +0430

    Added database getting migrated everytime  api starts

--- MineManagementApi/Program.cs
+++ MineManagementApi/Program.cs
@@ -4,13 +4,20 @@ using MineManagementApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<MineManagementApiDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("MineManagementApiDbConnectionString"));
 });
+
+//Migrate The database to the newest version
+using (MineManagementApiDbContext dbContext = new MineManagementApiDbContext(
+    new DbContextOptionsBuilder().UseSqlServer(builder.Configuration.GetConnectionString("MineManagementApiDbConnectionString")).Options))
+{
+    dbContext.Database.Migrate();
+}
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
